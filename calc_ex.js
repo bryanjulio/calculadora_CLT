42e45592144601bf44f23f200750b58d473102bf
Bryan Julio <bryan.19julio@gmail.com>
2023-10-12 00:53:21 -0300

    otimização para jQuery

--- calc_ex.js
+++ calc_ex.js
@@ -1,208 +1,117 @@
-// DADOS RESULTADOS
-const dados = [];
+$(document).ready(function() {
+    const elements = {
+        nome: $("#nome"),
+        salarioHora: $("#salarioHora"),
+        quantidadeHoras: $("#quantidadeHoras"),
+        quantidadeExtra: $("#quantidadeExtra"),
+        calcBtn: $("#calc-btn"),
+        clearBtn: $("#clear-btn"),
+        calcContainer: $("#calc-container"),
+        resultContainer: $("#result-container"),
+        nameContainer: $("#nomecontainer"),
+        salarioNumber: $("#salario-number span"),
+        valeNumber: $("#vale-number span"),
+        extraNumber: $("#extra-number span"),
+        pagamentoNumber: $("#pagamento-number span"),
+        salarioBrutoNumber: $("#salario-bruto"),
+        salarioInput: $("#salario-input span"),
+        horasInput: $("#horas-input span"),
+        extraInput: $("#extra-input span"),
+        nomeUser: $("#nome-usuario span"),
+        backBtn: $("#back-btn"),
+    };
 
-// Seleção de elementos
+    function updateResult() {
+        const nome = elements.nome.val();
+        const salarioHora = elements.salarioHora.val().replace(",", ".");
+        const quantidadeHoras = elements.quantidadeHoras.val().replace(",", ".");
+        const quantidadeExtra = elements.quantidadeExtra.val().replace(",", ".");
 
-const nomeInput = document.querySelector("#nome");
-const salarioHoraInput = document.querySelector("#salarioHora");
-const quantidadeHorasInput = document.querySelector("#quantidadeHoras");
-const quantidadeExtraInput = document.querySelector("#quantidadeExtra");
-const calcBtn = document.querySelector("#calc-btn");
-const clearBtn = document.querySelector("#clear-btn");
+        if (!salarioHora || !quantidadeHoras || !quantidadeExtra) return;
 
+        const resultadoSalario = (salarioHora * quantidadeHoras).toFixed(2);
+        const resultadoVale = (salarioHora * quantidadeHoras * 0.4).toFixed(2);
+        const resultadoTotalExtra = (salarioHora * quantidadeExtra).toFixed(2);
 
-const calcContainer = document.querySelector("#calc-container");
-const resultContainer = document.querySelector("#result-container");
-const nameContainer = document.querySelector("#nomecontainer")
+        const salario = salarioHora * quantidadeHoras;
+        let descontoInss = 0;
+        if (salario <= 1212) {
+            descontoInss = salario * 0.075;
+        } else if (salario <= 2427) {
+            descontoInss = salario * 0.09;
+        } else if (salario <= 3641) {
+            descontoInss = salario * 0.12;
+        } else {
+            descontoInss = salario * 0.14;
+        }
 
-const salarioNumber = document.querySelector("#salario-number span");
-const valeNumber = document.querySelector("#vale-number span");
-const extraNumber = document.querySelector("#extra-number span");
-const pagamentoNumber = document.querySelector("#pagamento-number span");
-const salarioBrutoNumber = document.querySelector("#salario-bruto")
+        const resultadoPagamento = (salario + salarioHora * quantidadeExtra - descontoInss).toFixed(2);
+        const resultadoBruto = ((salarioHora * quantidadeHoras) + (salarioHora * quantidadeExtra)).toFixed(2);
 
-const salarioInput = document.querySelector("#salario-input span");
-const horasInput = document.querySelector("#horas-input span");
-const extraInput = document.querySelector("#extra-input span");
+        elements.nomeUser.text(`Olá ${nome}!`);
+        elements.salarioNumber.text(resultadoSalario);
+        elements.valeNumber.text(resultadoVale);
+        elements.extraNumber.text(resultadoTotalExtra);
+        elements.pagamentoNumber.text(resultadoPagamento);
+        elements.salarioBrutoNumber.text(resultadoBruto);
 
-const nomeUser = document.querySelector("#nome-usuario span")
+        elements.salarioInput.text(salarioHora);
+        elements.horasInput.text(quantidadeHoras);
+        elements.extraInput.text(quantidadeExtra);
 
-const backBtn = document.querySelector("#back-btn");
-
-
-
-// Funções
-
-
-
-function createTable(dados) {
-    dados.innerText
-
-}
-
-function validDigits(text) {
-    return text.replace(/[^0-9,]/g, "");
-}
-
-function calcSalario(salarioHora, quantidadeHoras) {
-    const salario = (salarioHora * quantidadeHoras).toFixed(2);
-
-    return (salario);
-}
-
-function calcBruto(salarioHora, quantidadeHoras, quantidadeExtra) {
-    const totalExtra = salarioHora * quantidadeExtra;
-    const salarioBruto = (salarioHora * quantidadeHoras + totalExtra).toFixed(2);
-
-    return (salarioBruto);
-   
-}
-
-
-function calcVale(salarioHora, quantidadeHoras) {
-    const salario = salarioHora * quantidadeHoras;
-    const vale = salario * (40 / 100);
-
-    return (vale.toFixed(2));
-}
-
-function calcTotalExtra(salarioHora, quantidadeExtra) {
-    const totalExtra = (salarioHora * quantidadeExtra).toFixed(2);
-
-    return (totalExtra);
-}
-
-function calcPagamento(salarioHora, quantidadeHoras, quantidadeExtra) {
-    const totalExtra = salarioHora * quantidadeExtra;
-    let salario = salarioHora * quantidadeHoras;
-
-
-    if (salario <= 1212) {
-        descontoInss = salario * (7.5 / 100)
-        console.log(salario.toFixed(2))
-
-    } else if (salario > 1212 && salario <= 2427) {
-        descontoInss = salario * (9 / 100)
-
-    } else if (salario > 2427 && salario <= 3641) {
-        descontoInss = salario * (12 / 100)
-    } else {
-        descontoInss = salario * (14 / 100)
+        elements.calcContainer.addClass("hide");
+        elements.resultContainer.removeClass("hide");
+        elements.nameContainer.removeClass("hide");
     }
 
+    function cleanInputs() {
+        for (const key in elements) {
+            if (elements[key].is('input')) {
+                elements[key].val("");
+            }
+        }
 
-    const pagamento = (salario + totalExtra - descontoInss).toFixed(2);
+        elements.extraNumber.removeClass("hide");
+        elements.pagamentoNumber.removeClass("hide");
+        elements.nomeUser.removeClass("hide");
+        elements.calcContainer.removeClass("hide");
+        elements.resultContainer.addClass("hide");
+        elements.nameContainer.addClass("hide");
+    }
 
-    return (pagamento);
-}
+    elements.nome.keypress(function(e) {
+        const keyCode = e.keyCode ? e.keyCode : e.which;
+        if (keyCode > 47 && keyCode < 58) {
+            e.preventDefault();
+        }
+    });
 
+    $(document).keypress(function(e) {
+        if (e.key === "Enter") {
+            elements.calcBtn.click();
+        }
+    });
 
+    const inputs = [elements.salarioHora, elements.quantidadeHoras, elements.quantidadeExtra];
 
-function cleanInputs() {
-    nomeInput.value = "";
-    salarioHoraInput.value = "";
-    quantidadeHorasInput.value = "";
-    quantidadeExtraInput.value = "";
-    //salarioNumber.className = "";
-    //valeNumber.className = "";
-    extraNumber.className = "";
-    pagamentoNumber.className = "";
-    nomeUser.className = "";
-    
+    inputs.forEach(function(el) {
+        el.on("input", function(e) {
+            const updatedValue = el.val().replace(/[^0-9,]/g, "");
+            el.val(updatedValue);
+        });
+    });
 
-}
-
-function showOrHideResults() {
-    calcContainer.classList.toggle("hide");
-    resultContainer.classList.toggle("hide");
-    nameContainer.classList.toggle("hide");
-}
-
-
-// Init
-createTable(dados);
-
-
-
-// Eventos
-
-nomeInput.addEventListener("keypress", function (e) {
-    var keyCode = (e.keyCode ? e.keyCode : e.which);
-
-    if (keyCode > 47 && keyCode < 58) {
+    elements.calcBtn.click(function(e) {
         e.preventDefault();
-    }
-});
+        updateResult();
+    });
 
+    elements.clearBtn.click(function(e) {
+        e.preventDefault();
+        cleanInputs();
+    });
 
-
-document.addEventListener("keypress", function (e) {
-
-    if (e.key === "Enter") {
-
-        calcBtn.click();
-    }
-});
-
-
-
-[salarioHoraInput, quantidadeHorasInput, quantidadeExtraInput].forEach((el) => {
-    el.addEventListener("input", (e) => {
-        const updatedValue = validDigits(e.target.value);
-
-        e.target.value = updatedValue;
+    elements.backBtn.click(function(e) {
+        cleanInputs();
     });
 });
-
-calcBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const nome = nomeInput.value;
-    const salarioHora = salarioHoraInput.value.replace(",", ".");
-    const quantidadeHoras = quantidadeHorasInput.value.replace(",", ".");
-    const quantidadeExtra = quantidadeExtraInput.value.replace(",", ".");
-
-    console.log(nome, salarioHora, quantidadeHoras, quantidadeExtra);
-
-    if (!salarioHora || !quantidadeHoras || !quantidadeExtra) return;
-
-
-
-    const resultadoSalario = calcSalario(salarioHora, quantidadeHoras);
-    const resultadoVale = calcVale(salarioHora, quantidadeHoras);
-    const resultadoTotalExtra = calcTotalExtra(salarioHora, quantidadeExtra);
-    const resultadoPagamento = calcPagamento(salarioHora, quantidadeHoras, quantidadeExtra);
-    const resultadoBruto = calcBruto(salarioHora, quantidadeHoras, quantidadeExtra);
-
-
-
-
-    nomeUser.innerText = (`Olá ${nome}!`);
-    // salarioNumber.innerHTML = resultadoSalario;
-    //valeNumber.innerHTML = resultadoVale;
-    extraNumber.innerHTML = resultadoTotalExtra.replace(".", ",");
-    pagamentoNumber.innerHTML = resultadoPagamento.replace(".", ",");
-    salarioBrutoNumber.innerHTML = resultadoBruto.replace(".", ",");
-
-    salarioInput.innerHTML = salarioHoraInput.value;
-    horasInput.innerHTML = quantidadeHorasInput.value;
-    extraInput.innerHTML = quantidadeExtraInput.value;
-
-
-    showOrHideResults();
-
-});
-
-
-clearBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    cleanInputs();
-});
-
-
-backBtn.addEventListener("click", (e) => {
-    cleanInputs();
-    showOrHideResults();
-});
-
